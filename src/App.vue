5d6cb29ead7d1577c90e9581c6479dd2ab41c49a
Alan de Jesus Ayala Galvan <alan.ayala@facturatech.co>
2026-04-08 14:38:06 -0600

    feat: revert text component created

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import RevertTextComponent from "./components/RevertTextComponent.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <RevertTextComponent />
 </template>
---
+++ src/components/RevertTextComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <h1>Revert Text</h1>
+
+    <textarea v-model="text" placeholder="Type something..."></textarea>
+
+    <div class="buttons-container">
+      <button @click="clearInput">Clear</button>
+      <button class="submit" @click="handleSubmit">Submit</button>
+    </div>
+
+    <p v-if="showText">Texto enviado correctamente</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const text = ref("");
+const showText = ref(false);
+
+const clearInput = () => {
+  text.value = "";
+  showText.value = false;
+};
+
+const handleSubmit = () => {
+  if (text.value.trim() !== "") {
+    showText.value = true;
+  }
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  gap: 10px;
+}
+
+textarea {
+  width: 450px;
+  height: 100px;
+}
+
+.buttons-container {
+  margin-top: 16px;
+  display: flex;
+  justify-content: space-between;
+  gap: 16px;
+}
+
+button {
+  padding: 5px 10px;
+  padding: 12px 24px;
+  border: none;
+  border-radius: 16px;
+}
+
+.submit {
+  background-color: #53A653;
+}
+</style>
